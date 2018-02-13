b811e1e9539f650c058c76b67ba0d5ef616ab0c4
Horizon <Horizon@DESKTOP-788GFLD>
2018-02-13 19:35:12 +0200

    first

---
+++ src/Cal/test.java
@@ -0,0 +1,40 @@
+package Cal;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class test {
+	Model l=new Model();
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		//fail("Not yet implemented");
+		assertEquals((int)2.0,(int)l.Div(8.0,4.0));
+		assertEquals(1,(int)(l.Div(8.0,0.0)));
+		assertEquals(1,(int)(l.mod(8.0,0.0)));
+		assertEquals(1,(int)(l.sqr(-1)));
+
+
+	}
+
+}
